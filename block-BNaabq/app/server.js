a48431b04b260ee12a4549ef5f07109cddf0afc8
anandvipul <anandvipul01@gmail.com>
2022-12-26 11:51:22 +0530

    middleware intro

--- block-BNaabq/app/server.js
+++ block-BNaabq/app/server.js
@@ -1,15 +1,29 @@
 let express = require('express');
-let cookie = require('cookie-parser');
 const cookieParser = require('cookie-parser');
+let morgan = require('morgan');
 
 
 let app = express();
 
+
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(__dirname + "/public"));
 
+app.use(cookieParser());
+app.use("/about", (req, res, next) => {
+    res.cookie("username", "amigos");
+    res.end("About Page");
+    next();
+});
 
+app.use((req, res, next) => {
+    console.log(req.cookies);
+    next();
+});
+
+app.use(morgan("combined"));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
